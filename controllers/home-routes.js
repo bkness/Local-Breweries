2872c989ba4263892baff60323efb177c289710b
Brandon Kelly <kbrandon863@gmail.com>
2026-03-12 01:24:06 -0700

    Changed api endpoint to current open brewery db api, app works

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,57 +1,59 @@
-const router = require('express').Router();
-const { Breweries, User } = require('../models');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const { Breweries, User } = require("../models");
+const withAuth = require("../utils/auth");
 
-router.get('/', async (req, res) => {
-    try {
-        const postData = await Breweries.findAll({
-            include: [User]
-        })
+router.get("/", async (req, res) => {
+  try {
+    const postData = await Breweries.findAll({
+      include: [User],
+    });
 
-        const brewery = postData.map(brewery => brewery.get({ plain: true })) // session of object is global to backend
+    const brewery = postData.map((brewery) => brewery.get({ plain: true })); // session of object is global to backend
 
-        const breweryData = await fetch('https://api.openbrewerydb.org/breweries?by_city=phoenix');
-        const apiData = await breweryData.json();
+    const breweryData = await fetch(
+      "https://api.openbrewerydb.org/v1/breweries?by_city=phoenix",
+    );
+    const apiData = await breweryData.json();
 
-        // console.log(apiData)
+    // console.log(apiData)
 
-        // res.json(apiData)
-        res.render('homepage', {
-            apiData,
-            logged_in: req.session.logged_in
-        })
-    } catch (err) {
-        console.log(err.message)
-        res.status(500).json(err.message)
-    }
-})
+    // res.json(apiData)
+    res.render("homepage", {
+      apiData,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json(err.message);
+  }
+});
 
-router.get('/citySearch/:searchCity', withAuth, async (req, res) => {
-    try {
-        //console.log(req.params.searchCity);
-        const brewUrl = `https://api.openbrewerydb.org/breweries?by_city=${req.params.searchCity}`
-        const breweryData = await fetch(brewUrl);
-        const apiData = await breweryData.json();
+router.get("/citySearch/:searchCity", withAuth, async (req, res) => {
+  try {
+    //console.log(req.params.searchCity);
+    const brewUrl = `https://api.openbrewerydb.org/v1/breweries?by_city=${req.params.searchCity}`;
+    const breweryData = await fetch(brewUrl);
+    const apiData = await breweryData.json();
 
-        //console.log('hi')
+    //console.log('hi')
 
-        //res.json(apiData)
-        res.render('homepage', {
-            apiData,
-            logged_in: req.session.logged_in
-        })
-    } catch (err) {
-        console.log(err.message)
-        res.status(500).json(err.message)
-    }
-})
+    //res.json(apiData)
+    res.render("homepage", {
+      apiData,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json(err.message);
+  }
+});
 
-router.get('/login', (req, res) => {
-    if (req.session.logged_in) {
-        res.redirect('homepage');
-        return;
-    }
-    res.render('login')
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("homepage");
+    return;
+  }
+  res.render("login");
 });
 
 module.exports = router;
